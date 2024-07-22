a54970d03c076a7f2caf2807547818b8affabe55
Pablo Giress <pablogiress@gmail.com>
2024-07-21 20:11:10 -0500

    Commit initial

---
+++ src/main/java/com/pablogiress/api_tekos/ApiTekosApplication.java
@@ -0,0 +1,13 @@
+package com.pablogiress.api_tekos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTekosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTekosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablogiress/api_tekos/ApiTekosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablogiress.api_tekos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTekosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
